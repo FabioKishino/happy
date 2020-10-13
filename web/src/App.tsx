9cd0f645b7b387b1c146f5bed07127077c917b4a
Fabio Kishino <fabiokishino@hotmail.com>
2020-10-13 16:57:42 -0300

    Front-End Initial commit
    Front-End Initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles/global.css';
+
+import Routes from './routes';
+// JSX - JavaScript XML
+// Compomente: Funcao JS que retorna HTML
+// Passar atributo para um componente -> propriedade
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
